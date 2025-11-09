58a65f803691b05fe5a4843d6a4e634f3cd62a3c
tokKurumi <tkurumi901@gmail.com>
2025-11-09 06:28:02 +0300

    feat: update prompt.service launch configuration to use environment variables

--- services/prompt.service/src/include/configuration/config.h
+++ services/prompt.service/src/include/configuration/config.h
@@ -8,19 +8,18 @@ struct SshConfig
 {
     // PROMPT_BIND_ADDR
     // address to bind SSH server
-    std::string bind_addr{"0.0.0.0"};
+    std::string bind_addr;
 
     // PROMPT_BIND_PORT
     // port to bind SSH server
-    std::uint16_t bind_port{2222};
+    std::uint16_t bind_port;
 
-    // PROMPT_SSH_RSA_HOST_PUBLIC_KEY_PATH (optional)
-    // path to host public key (optional; used for logging / distribution to clients)
+    // PROMPT_SSH_RSA_HOST_PUBLIC_KEY_PATH
+    // path to host public key
     std::string rsa_host_public_key_path;
 
     // PROMPT_SSH_RSA_HOST_PRIVATE_KEY_PATH
     // path to RSA private host key file for SSH server
-    // can be set via env var or as docker secret path (e.g., /run/secrets/prompt_ssh_rsa_key)
     std::string rsa_host_private_key_path;
 };
 
@@ -28,7 +27,7 @@ struct ThreadPoolConfig
 {
     // PROMPT_THREADS
     // number of threads in the pool
-    int threads{4};
+    int threads;
 };
 
 struct AppConfig
